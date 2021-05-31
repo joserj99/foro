512a77dbdbde9142cd2c3a15ed4fc459aaefbab2
joserj99 <64386684+joserj99@users.noreply.github.com>
2021-05-31 15:02:07 -0500

    Update TemasController.java

--- src/main/java/com/dwa/foro/controladores/TemasController.java
+++ src/main/java/com/dwa/foro/controladores/TemasController.java
@@ -58,7 +58,7 @@ public class TemasController {
 		if(!servicioTema.agregar(tema)) {
 			modelo.addAttribute("error", servicioTema.getMensaje());
 		}
-		return "redirect:temas/listar";
+		return "redirect:/temas/listar";
 	}
 	
 	@GetMapping("/temas/detalles/{id}")
@@ -94,6 +94,6 @@ public class TemasController {
 		if(!servicioReplica.agregar(replica)) {
 			modelo.addAttribute("error", servicioReplica.getMensaje());
 		}
-		return "redirect:temas/detalles/" + replica.getTema().getId();
+		return "redirect:/temas/detalles/" + replica.getTema().getId();
 	}
 }
